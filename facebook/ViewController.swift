2876180c32d8a02e3025f78ea064f47de9a43e69
zhoujun0920 <zhoujun199009@hotmail.com>
2015-06-18 23:13:52 -0500

    Initial Commit

---
+++ facebook/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  facebook
+//
+//  Created by Jun Zhou on 6/18/15.
+//  Copyright (c) 2015 Jun Zhou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
